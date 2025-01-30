02dce48a4297ada0582ea7e54e74f4c4996ca7fb
PikaKight <tmmarcus73@gmail.com>
2025-01-30 12:53:57 -0500

    temp

--- Assignment 1/README.md
+++ Assignment 1/README.md
@@ -8,4 +8,4 @@ The code creates a window of 1280 px by 1000 px titled `Hello World` and opens t
 
 While the window is opened, it checks for all pending events and then clear the active color. Then it sets the active color to Azure RGB(0, 50, 100).
 
-Next it draws a polygon with the given vertices to create an Azure triangle. Final it clears the buffers and repeats until the window is closed.
+Next it draws a Triangle with the given vertices to create an Azure triangle. Final it clears the buffers and repeats until the window is closed.
--- Assignment 1/first.cpp
+++ Assignment 1/first.cpp
@@ -2,14 +2,14 @@
 
 int main(void)
 {
-    GLFWwindow* window;
+    GLFWwindow *window;
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = //...
+    window = glfwCreateWindow(1280, 1000, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -19,21 +19,28 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Poll for and process events */
         glfwPollEvents();
-        
-		/* Render here */
+
+        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-		//...
-        
-		/* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glColor3f(0.0, 50.0, 100.0);
 
+        glBegin(GL_TRIANGLES);
+
+        glVertex2f(0, 0.5);
+        glVertex2f(0.5, -0.25);
+        glVertex2f(-0.5, -0.25);
+
+        glEnd();
+        glFlush();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
     }
 
     glfwTerminate();
