bf42c84713126aa8feee73f683245a246141f4d9
PikaKight <tmmarcus73@gmail.com>
2025-03-23 15:05:43 -0400

    working...

--- Assignment5/main.cpp
+++ Assignment5/main.cpp
@@ -18,12 +18,7 @@
 using namespace std;
 using namespace glm;
 
-Camera camera;
-
-void framebuffer_size_callback(GLFWwindow *window, int w, int h)
-{
-    glViewport(0, 0, w, h);
-}
+Camera camera; 
 
 GLuint loadShader()
 {
@@ -33,30 +28,55 @@ GLuint loadShader()
 
     string vertexShaderSource = R"(
         #version 330 core
-        layout (location = 0) in vec3 aPos;
-        layout (location = 1) in vec3 aNormal;
-        layout (location = 3) in vec2 aTexCoord;
-        
-        uniform mat4 MVP;
-        
-        out vec2 TexCoord;
-        
+        layout(location = 0) in vec3 position;
+        layout(location = 1) in vec3 normal;
+
+        uniform mat4 MVP;  // Model-view-projection matrix
+        uniform mat4 V;    // View matrix
+        uniform vec3 LightDir;  // Light direction
+
+        out vec3 fragNormal;
+        out vec3 fragLightDir;
+        out vec3 fragViewDir;
+
         void main() {
-            gl_Position = MVP * vec4(aPos, 1.0);
-            TexCoord = aTexCoord;
+            gl_Position = MVP * vec4(position, 1.0);
+            
+            fragNormal = normalize(mat3(V) * normal);  // Transform normal to view space
+            fragLightDir = normalize(LightDir);  // Light direction in view space
+            fragViewDir = normalize(-vec3(V * vec4(position, 1.0)));  // View direction
         }
         )";
 
     string fragmentShaderSource = R"(
         #version 330 core
-        out vec4 FragColor;
-        
-        in vec2 TexCoord;
-        uniform sampler2D texture1;
-        
-        void main() {
+        in vec3 fragNormal;
+        in vec3 fragLightDir;
+        in vec3 fragViewDir;
 
-            FragColor = texture(texture1, TexCoord);
+        uniform vec3 modelColor;  // Base color for diffuse component
+        uniform vec3 ambientColor;  // Ambient light color
+        uniform vec3 specularColor;  // Specular light color
+        uniform float shininess;  // Shininess factor
+
+        out vec4 FragColor;
+
+        void main() {
+            // Ambient component
+            vec3 ambient = ambientColor * modelColor;
+
+            // Diffuse component (Lambertian reflection)
+            float diff = max(dot(fragNormal, fragLightDir), 0.0);
+            vec3 diffuse = diff * modelColor;
+
+            // Specular component (Phong reflection)
+            vec3 reflectDir = reflect(-fragLightDir, fragNormal);
+            float spec = pow(max(dot(fragViewDir, reflectDir), 0.0), shininess);
+            vec3 specular = spec * specularColor;
+
+            // Final color
+            vec3 finalColor = ambient + diffuse + specular;
+            FragColor = vec4(finalColor, 1.0);
         }
         )";
 
@@ -98,6 +118,131 @@ GLuint loadShader()
     return shaderProgram;
 }
 
+float lastX = 400, lastY = 300;
+bool firstMouse = true;
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+    
+    float xOffset = xpos - lastX;
+    float yOffset = lastY - ypos;  // reversed because y-coordinates go from bottom to top
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.processMouse(xOffset, yOffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+    camera.processScroll(yoffset);
+}
+
+void drawBoundaryBox(float min, float max){
+    GLfloat vertices[] = {
+        min, min, min,  min, min, max,  min, max, min,  min, max, max,  // front bottom-left, top-left
+        max, min, min,  max, min, max,  max, max, min,  max, max, max,  // back bottom-right, top-right
+        min, min, min,  max, min, min,  min, max, min,  max, max, min   // bottom-left to top-right edges
+    };
+
+    GLuint indices[] = {
+        0, 1, 1, 3, 3, 2, 2, 0, 4, 5, 5, 7, 7, 4, 1, 5, 3, 7, 2, 6, 6, 0
+    };
+
+    GLuint VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    glBindVertexArray(0);
+
+    // Render the bounding box
+    glBindVertexArray(VAO);
+    glDrawElements(GL_LINES, 24, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+}
+
+void drawAxes(float min, float max) {
+    GLfloat axesVertices[] = {
+        min, min, min,  max, min, min,  // X-axis (Red)
+        min, min, min,  min, max, min,  // Y-axis (Green)
+        min, min, min,  min, min, max   // Z-axis (Blue)
+    };
+
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(axesVertices), axesVertices, GL_STATIC_DRAW);
+
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
+    // Render the axes
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_LINES, 0, 6);
+    glBindVertexArray(0);
+}
+
+void drawMarchingCubes(const vector<float>& vertices, const vector<float>& normals) {
+    GLuint VBO, VAO, NBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &NBO);
+
+    glBindVertexArray(VAO);
+
+    // Store vertices
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW);
+
+    // Store normals
+    glBindBuffer(GL_ARRAY_BUFFER, NBO);
+    glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(float), &normals[0], GL_STATIC_DRAW);
+
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    // Normal attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
+    // Draw the cubes
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, vertices.size() / 3);
+    glBindVertexArray(0);
+}
+
+
 int main(int argc, char **argv)
 {
 
@@ -116,15 +261,23 @@ int main(int argc, char **argv)
     GLFWwindow *window = glfwCreateWindow(width, height, "Marching Cubes", NULL, NULL);
 
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     glewInit();
-    glEnable(GL_DEPTH_TEST);
 
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
 
     GLuint shaderID = loadShader();
+
+    float isovalue = 0.0f, min = -1.5f, max =  1.5f, stepsize = 0.1f;
+
+    auto vertices = marching_cubes([](float x, float y, float z) {
+        return x*x + y*y + z*z - 1.0f;  // Sphere function
+    }, 0.0f, -1.5f, 1.5f, 0.1f);
+
+    vector<float> normals = compute_normals(vertices);
 
     unsigned int VAO, VBO;
     glGenVertexArrays(1, &VAO);
@@ -137,21 +290,33 @@ int main(int argc, char **argv)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
+    glEnable(GL_DEPTH_TEST);
+
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
+        glm::mat4 model = glm::mat4(1.0f);
         mat4 view = camera.getViewMatrix();
         mat4 projection = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
-        mat4 MVP = projection * view;
+        mat4 MVP = projection * view * model;
 
         glUseProgram(shaderID);
-        glUniformMatrix4fv(glGetUniformLocation(shaderID, "MVP"), 1, GL_FALSE, value_ptr(MVP));
-        glUniform3f(glGetUniformLocation(shaderID, "modelColor"), 1.0f, 0.5f, 0.3f);
 
-        
+        glUniformMatrix4fv(glGetUniformLocation(shaderID, "MVP"), 1, GL_FALSE, glm::value_ptr(MVP));
+        glUniformMatrix4fv(glGetUniformLocation(shaderID, "V"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniform3f(glGetUniformLocation(shaderID, "LightDir"), 1.0f, 1.0f, -1.0f);
+        glUniform3f(glGetUniformLocation(shaderID, "modelColor"), 0.8f, 0.2f, 0.2f);
+
+        // Draw the bounding box and axes
+        drawBoundaryBox(min, max);
+        drawAxes(min, max);
+
+        // Draw the marching cubes
+        drawMarchingCubes(vertices, normals);
+
         glfwSwapBuffers(window);
     }
 
--- Assignment5/marching_cubes.cpp
+++ Assignment5/marching_cubes.cpp
@@ -6,6 +6,67 @@
 using namespace std;
 using namespace glm;
 
+struct Vec3 {
+    float x, y, z;
+
+    Vec3() : x(0), y(0), z(0) {}
+    Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
+
+    // Vector subtraction
+    Vec3 operator-(const Vec3& other) const {
+        return Vec3(x - other.x, y - other.y, z - other.z);
+    }
+
+    // Cross product
+    Vec3 cross(const Vec3& other) const {
+        return Vec3(
+            y * other.z - z * other.y,
+            z * other.x - x * other.z,
+            x * other.y - y * other.x
+        );
+    }
+
+    // Normalize the vector
+    void normalize() {
+        float length = sqrt(x * x + y * y + z * z);
+        if (length > 0) {
+            x /= length;
+            y /= length;
+            z /= length;
+        }
+    }
+};
+
+vector<float> compute_normals(const vector<float>& vertices) {
+    vector<float> normals;
+    int numVertices = vertices.size() / 3;
+    normals.resize(vertices.size());
+
+    for (int i = 0; i < numVertices; i += 3) {
+        // Get three vertices of the triangle
+        Vec3 v0(vertices[i], vertices[i + 1], vertices[i + 2]);
+        Vec3 v1(vertices[i + 3], vertices[i + 4], vertices[i + 5]);
+        Vec3 v2(vertices[i + 6], vertices[i + 7], vertices[i + 8]);
+
+        // Compute two edges
+        Vec3 edge1 = v1 - v0;
+        Vec3 edge2 = v2 - v0;
+
+        // Cross product to get the normal
+        Vec3 normal = edge1.cross(edge2);
+        normal.normalize();  // Normalize the normal
+
+        // Store the same normal for all three vertices of the triangle
+        for (int j = 0; j < 3; ++j) {
+            normals[i + j * 3] = normal.x;
+            normals[i + j * 3 + 1] = normal.y;
+            normals[i + j * 3 + 2] = normal.z;
+        }
+    }
+    
+    return normals;
+}
+
 vector<float> marching_cubes(
     function<float(float, float, float)> f,
     float isovalue, float min, float max, float stepsize) {
